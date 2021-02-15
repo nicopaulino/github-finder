d7e1d6c0bf16de6608c0d369ff639d290b124c89
Nico Paulino <nicopaulino8@gmail.com>
2021-02-15 14:36:03 -0600

    Add Background Color

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App className="page"/>,
   document.getElementById('root')
 );
 
