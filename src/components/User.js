525f06948eaf5ec5d30ccc1c5d9585801b4ce219
Nico Paulino <nicopaulino8@gmail.com>
2021-02-15 14:40:50 -0600

    Add useStyles to Toggle Button

--- src/components/User.js
+++ src/components/User.js
@@ -11,3 +11,16 @@ import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import axios from 'axios';
 
+
+const useStyles = makeStyles((theme) => ({
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+}));
