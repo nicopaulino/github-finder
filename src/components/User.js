de8d53413c955dae6891e102a1fc2c1cc4c63db1
Nico Paulino <nicopaulino8@gmail.com>
2021-02-15 14:40:22 -0600

    Import Dependencies

---
+++ src/components/User.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from '@material-ui/core/Card';
+import './User.css';
+import Collapse from '@material-ui/core/Collapse';
+import Typography from '@material-ui/core/Typography';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import axios from 'axios';
+
